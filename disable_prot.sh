1bfe1303d5cc2bfb2d925f00d7d18a5950c2f4ec
toni <toni@devlap.local>
2015-03-01 14:29:57 +0100

    - readlink should do the trick

--- disable_prot.sh
+++ disable_prot.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ `id -u` -ne 0 ]; then
-  echo "$0: This program should be run as root"
+  echo "$0: This script should be run as root"
   echo "$0: Try to get root .."
-  su -l root -c "$(realpath $0)"
+  su -l root -c "$(readlink -f $0)"
   exit $?
 fi
 
